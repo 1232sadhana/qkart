6c0683725fb74f5af387c30c63baab78bdcf3812
CrioUser <crio-user@criodo.com>
2023-10-23 19:28:10 +0530

    “ME_QKART_FRONTEND_V2_MODULE_LOGIN”

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,70 +1,126 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSnackbar } from "notistack";
 import axios from "axios";
-import { Search, SentimentDissatisfied } from "@mui/icons-material";
-import { Box, CircularProgress, Grid, InputAdornment, TextField } from "@mui/material";
+import { Search } from "@mui/icons-material";
+import { Grid, InputAdornment, TextField } from "@mui/material";
 import { config } from "../App";
-import Header from "./Header";
-import Cart from "./Cart";
+import Header from "./Header"; // Removed reference to ProductCard component
 
 const Products = () => {
   const token = localStorage.getItem("token");
   const { enqueueSnackbar } = useSnackbar();
-  const [loading, setLoading] = useState(false);
-  const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [debounceTimeout, setDebounceTimeout] = useState(null);
-  const [items, setItems] = useState([]);
 
   const debounceSearch = (event, debounceTimeout) => {
-    // Debounce search functionality
-    // Implement this logic
+    const value = event.target.value;
+    if (debounceTimeout) {
+      clearTimeout(debounceTimeout);
+    }
+    const timeout = setTimeout(() => {
+      performSearch(value);
+    }, 500);
+    setDebounceTimeout(timeout);
   };
 
   useEffect(() => {
-    // Fetch the products and initial cart data
-    // Implement this logic
-  }, []);
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get(`${config.endpoint}/products`);
+        setFilteredProducts(response.data);
+      } catch (e) {
+        if (e.response && e.response.status === 500) {
+          enqueueSnackbar(e.response.data.message, { variant: "error" });
+        } else {
+          enqueueSnackbar("Something went wrong!", { variant: "error" });
+        }
+      }
+    };
+
+    fetchProducts();
+  }, [enqueueSnackbar]);
+
+  const performSearch = async (text) => {
+    try {
+      const response = await axios.get(
+        `${config.endpoint}/products/search?value=${text}`
+      );
+      setFilteredProducts(response.data);
+    } catch (e) {
+      if (e.response) {
+        if (e.response.status === 404) {
+          setFilteredProducts([]);
+        }
+        if (e.response.status === 500) {
+          enqueueSnackbar(e.response.data.message, { variant: "error" });
+          setFilteredProducts(filteredProducts);
+        }
+      } else {
+        enqueueSnackbar("Something went wrong!", { variant: "error" });
+      }
+    }
+  };
 
   const displayProducts = () => {
-    // Display products
-    // Implement this logic
+    return (
+      <Grid container margin="1rem" padding="1rem" spacing={2}>
+        {filteredProducts.length ? (
+          filteredProducts.map((product) => (
+            <Grid item xs={6} md={3} key={product._id}>
+              {/* ProductCard component removed */}
+            </Grid>
+          ))
+        ) : (
+          <div>
+            <h4 style={{ color: "#636363" }}>No Products found</h4>
+          </div>
+        )}
+      </Grid>
+    );
   };
 
   return (
     <div>
       <Header>
-        {/* Header with logo */}
-        {/* Add your header logo here */}
         <img src="logo-url" alt="Logo" className="header-logo" />
 
         {token ? (
           <div>
-            {/* Logged in: Display username & avatar */}
             <span className="header-username">Your Username</span>
             <img src="avatar-url" alt="User Avatar" className="header-avatar" />
 
-            {/* Logged in: Logout button */}
-            <button className="header-logout-button" onClick={handleLogout}>
+            <button
+              className="header-logout-button"
+              onClick={() => {
+                localStorage.clear();
+                window.location.reload(); // Refresh the page
+              }}
+            >
               Logout
             </button>
           </div>
         ) : (
           <div>
-            {/* When logged out: Login button */}
-            <button className="header-login-button" onClick={handleLogin}>
+            <button
+              className="header-login-button"
+              onClick={() => {
+                // Implement your logic to route to the login page
+              }}
+            >
               Login
             </button>
-
-            {/* When logged out: Register button */}
-            <button className="header-register-button" onClick={handleRegister}>
+            <button
+              className="header-register-button"
+              onClick={() => {
+                // Implement your logic to route to the register page
+              }}
+            >
               Register
             </button>
           </div>
         )}
       </Header>
 
-      {/* Search bar */}
       <TextField
         className="search-desktop"
         size="small"
@@ -98,20 +154,15 @@ const Products = () => {
       />
 
       <Grid container>
-        <Grid container>
-          {token ? (
-            <Grid container>
-              <Grid item xs={12} md={9}>
-                {displayProducts()}
-              </Grid>
-              <Grid item xs={12} md={3}>
-                <Cart products={products} items={items} handleQuantity={addToCart} />
-              </Grid>
+        {token ? (
+          <Grid container>
+            <Grid item xs={12} md={9}>
+              {displayProducts()}
             </Grid>
-          ) : (
-            displayProducts()
-          )}
-        </Grid>
+          </Grid>
+        ) : (
+          displayProducts()
+        )}
       </Grid>
     </div>
   );
